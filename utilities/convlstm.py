65f6c69723a74b232c236900dc7a5952800eb2b6
Markus Ernst <markusrolandernst@t-online.de>
2020-03-30 09:34:17 +0200

    merged with codetemplate

--- utilities/convlstm.py
+++ utilities/convlstm.py
@@ -1,5 +1,58 @@
+#!/usr/bin/python
+#
+# Project Titan
+# _____________________________________________________________________________
+#
+#                                                                         _.oo.
+# March 2020                                    _.u[[/;:,.         .odMMMMMM'
+#                                             .o888UU[[[/;:-.  .o@P^    MMM^
+# convlstm.py                                oN88888UU[[[/;::-.        dP^
+# Description Description                   dNMMNN888UU[[[/;:--.   .o@P^
+# Description Description                  ,MMMMMMN888UU[[/;::-. o@^
+#                                          NNMMMNN888UU[[[/~.o@P^
+# Markus Ernst                             888888888UU[[[/o@^-..
+#                                         oI8888UU[[[/o@P^:--..
+#                                      .@^  YUU[[[/o@^;::---..
+#                                    oMP     ^/o@P^;:::---..
+#                                 .dMMM    .o@^ ^;::---...
+#                                dMMMMMMM@^`       `^^^^
+#                               YMMMUP^
+#                                ^^
+# _____________________________________________________________________________
+#
+#
+# Copyright 2020 Markus Ernst
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# _____________________________________________________________________________
+
+
+# ----------------
+# import libraries
+# ----------------
+
+# standard libraries
+# -----
 import torch.nn as nn
 import torch
+
+# custom functions
+# -----
+
+# -----------------
+# ConvLSTM Classes
+# -----------------
 
 
 class ConvLSTMCell(nn.Module):
@@ -189,3 +242,16 @@ class ConvLSTM(nn.Module):
         if not isinstance(param, list):
             param = [param] * num_layers
         return param
+
+
+# _____________________________________________________________________________
+
+
+# -----------------
+# top-level comment
+# -----------------
+
+# medium level comment
+# -----
+
+# low level comment
