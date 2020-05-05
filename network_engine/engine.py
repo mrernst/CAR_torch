5230a7422f588514c88792d76fa4bf976c2cd7db
Markus Ernst <markusrolandernst@t-online.de>
2020-05-05 15:40:52 +0000

    First iteration, not yet usable

---
+++ network_engine/engine.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+#
+# Project Titan
+# _____________________________________________________________________________
+#
+#                                                                         _.oo.
+# April 2020                                     _.u[[/;:,.         .odMMMMMM'
+#                                             .o888UU[[[/;:-.  .o@P^    MMM^
+# engine.py                                  oN88888UU[[[/;::-.        dP^
+# Description Description                   dNMMNN888UU[[[/;:--.   .o@P^
+# Description Description                  ,MMMMMMN888UU[[/;::-. o@^
+#                                          NNMMMNN888UU[[[/~.o@P^
+# Markus Ernst                             888888888UU[[[/o@^-..
+#                                         oI8888UU[[[/o@P^:--..
+#                                      .@^  YUU[[[/o@^;::---..
+#                                    oMP     ^/o@P^;:::---..
+#                                 .dMMM    .o@^ ^;::---...
+#                                dMMMMMMM@^`       `^^^^
+#                               YMMMUP^
+#                                ^^
+# _____________________________________________________________________________
+#
+#
+# Copyright 2020 Markus Ernst
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# _____________________________________________________________________________
+
+
+# ----------------
+# import libraries
+# ----------------
+
+# standard libraries
+# -----
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+
+from torchvision import transforms, utils
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+
+import argparse
+import os
+import random
+import time
+import math
+
+# custom functions
+# -----
+
+import utilities.helper as helper
+# network structure
+from utilities.networks.convlstm import ConvLSTMCell, ConvLSTM
+# dataset
+from utilities.data_handler import dynaMODataset, ToTensor
+
+
+# cross-platform development
+from platform import system
+IS_MACOSX = True if system() == 'Darwin' else False
+PWD_STEM = "/Users/markus/Research/Code/" if IS_MACOSX else "/home/mernst/git/"
+
+# commandline arguments
+# -----
+
+# FLAGS
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+     "-t",
+     "--testrun",
+     # type=bool,
+     default=False,
+     dest='testrun',
+     action='store_true',
+     help='reduced dataset configuration on local machine for testing')
+parser.add_argument(
+     "-c",
+     "--config_file",
+     type=str,
+     default=PWD_STEM +
+             'saturn/experiments/001_noname_experiment/' +
+             'files/config_files/config0.csv',
+     help='path to the configuration file of the experiment')
+parser.add_argument(
+     "-n",
+     "--name",
+     type=str,
+     default='',
+     help='name of the run, i.e. iteration1')
+parser.add_argument(
+     "-r",
+     "--restore_ckpt",
+     type=bool,
+     default=True,
+     help='restore model from last checkpoint')
+
+FLAGS = parser.parse_args()
+
+
+
+print(FLAGS.testrun)
+
+
+# CONFIG = helper.infer_additional_parameters(
+#     helper.read_config_file(FLAGS.config_file)
+# )
+
+
+# _____________________________________________________________________________
+
+
+# -----------------
+# top-level comment
+# -----------------
+
+# medium level comment
+# -----
+
+# low level comment
