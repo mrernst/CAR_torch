f7662e98932603039206f5fdcb47a1da13043477
Markus Ernst <markusrolandernst@t-online.de>
2020-12-04 10:25:26 +0000

    less printout

--- network_engine/engine.py
+++ network_engine/engine.py
@@ -559,7 +559,9 @@ elif CONFIG['dataset'] == 'osycb2':
     print(len(train_dataset) + len(test_dataset))
     print(len(train_dataset))
     print(len(test_dataset))
-    print(test_dataset.paths_to_left_samples)
+    print(test_dataset.paths_to_left_samples[:25])
+    print(test_dataset.paths_to_left_samples[25:])
+
     raise NotImplementedError("osycb2 is not yet implemented")
 else:
     # Datasets LMDB Style
