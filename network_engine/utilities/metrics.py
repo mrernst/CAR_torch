2a33ce521255f1ebe8adc1ea523d46f1fb331edd
Markus Ernst <markusrolandernst@t-online.de>
2021-07-16 09:46:43 +0000

    rename distancemetrics module to include gini

--- network_engine/utilities/metrics.py
+++ network_engine/utilities/metrics.py
@@ -6,9 +6,9 @@
 #                                                                         _.oo.
 # April 2020                                    _.u[[/;:,.         .odMMMMMM'
 #                                             .o888UU[[[/;:-.  .o@P^    MMM^
-# distancemetrics.py                         oN88888UU[[[/;::-.        dP^
+# metrics.py                                 oN88888UU[[[/;::-.        dP^
 # a collection of                           dNMMNN888UU[[[/;:--.   .o@P^
-# distance metrics for tsne                ,MMMMMMN888UU[[/;::-. o@^
+# metrics for                             ,MMMMMMN888UU[[/;::-. o@^
 #                                          NNMMMNN888UU[[[/~.o@P^
 # Markus Ernst                             888888888UU[[[/o@^-..
 #                                         oI8888UU[[[/o@P^:--..
@@ -54,6 +54,33 @@ import numpy as np
 import hashlib
 
 from fractions import Fraction
+
+
+# calculate the gini coefficient from a numpy array
+def gini(array):
+    """Calculate the Gini coefficient of a numpy array."""
+    # based on bottom eq:
+    # http://www.statsdirect.com/help/generatedimages/equations/equation154.svg
+    # from:
+    # http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    # All values are treated equally, arrays must be 1d:
+    array = array.flatten()
+    if np.amin(array) < 0:
+        # Values cannot be negative:
+        array -= np.amin(array)
+    # Values cannot be 0:
+    array += 0.0000001
+    # Values must be sorted:
+    array = np.sort(array)
+    # Index per array element:
+    index = np.arange(1,array.shape[0]+1)
+    # Number of array elements:
+    n = array.shape[0]
+    # Gini coefficient:
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)))
+
+
+
 
 memoization = {}
 
