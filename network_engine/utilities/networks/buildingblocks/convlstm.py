b40a19a73c485de8c4745d62333497dfea5145c3
Markus Ernst <markusrolandernst@t-online.de>
2021-10-22 13:20:47 +0000

    added description for convlstm

--- network_engine/utilities/networks/buildingblocks/convlstm.py
+++ network_engine/utilities/networks/buildingblocks/convlstm.py
@@ -7,8 +7,8 @@
 # March 2020                                    _.u[[/;:,.         .odMMMMMM'
 #                                             .o888UU[[[/;:-.  .o@P^    MMM^
 # convlstm.py                                oN88888UU[[[/;::-.        dP^
-# Description Description                   dNMMNN888UU[[[/;:--.   .o@P^
-# Description Description                  ,MMMMMMN888UU[[/;::-. o@^
+# basic implementation of                   dNMMNN888UU[[[/;:--.   .o@P^
+# a convolutional lstm network             ,MMMMMMN888UU[[/;::-. o@^
 #                                          NNMMMNN888UU[[[/~.o@P^
 # Markus Ernst                             888888888UU[[[/o@^-..
 #                                         oI8888UU[[[/o@P^:--..
